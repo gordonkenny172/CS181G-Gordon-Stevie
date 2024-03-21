48bb2667210e31dd3d27e95f141a6097e73a9aef
sjkim994 <sjkd2022@mymail.pomona.edu>
2024-03-21 10:46:13 -0700

    bin package fighter pt 2

--- engine/target/debug/deps/engine-2825f202537e8ec9.d
+++
@@ -1,5 +0,0 @@
-/Users/skim/VSCode/cs181G/CS181G-Gordon-Stevie/engine/target/debug/deps/libengine-2825f202537e8ec9.rmeta: src/main.rs
-
-/Users/skim/VSCode/cs181G/CS181G-Gordon-Stevie/engine/target/debug/deps/engine-2825f202537e8ec9.d: src/main.rs
-
-src/main.rs:
--- engine/target/debug/deps/engine-2eb432c5c5243a3c.d
+++
@@ -1,5 +0,0 @@
-/Users/skim/VSCode/cs181G/CS181G-Gordon-Stevie/engine/target/debug/deps/libengine-2eb432c5c5243a3c.rmeta: src/main.rs
-
-/Users/skim/VSCode/cs181G/CS181G-Gordon-Stevie/engine/target/debug/deps/engine-2eb432c5c5243a3c.d: src/main.rs
-
-src/main.rs:
